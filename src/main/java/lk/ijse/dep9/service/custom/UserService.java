aa8b1c7359de41a39dd722498f1c2eecb229f49a
danulatb <balsooriyadt@gmail.com>
2022-11-29 19:30:14 +0530

    add service exceptions

--- src/main/java/lk/ijse/dep9/service/custom/UserService.java
+++ src/main/java/lk/ijse/dep9/service/custom/UserService.java
@@ -1,6 +1,14 @@
 package lk.ijse.dep9.service.custom;
 
+import lk.ijse.dep9.dto.UserDTO;
 import lk.ijse.dep9.service.SuperService;
 
 public interface UserService extends SuperService {
+
+    void createNewUser(UserDTO dto);
+
+    void removeUser(String userName);
+
+    UserDTO getUserDetails(String userName);
+
 }
--- src/main/java/lk/ijse/dep9/service/custom/impl/UserServiceImpl.java
+++ src/main/java/lk/ijse/dep9/service/custom/impl/UserServiceImpl.java
@@ -3,4 +3,6 @@ package lk.ijse.dep9.service.custom.impl;
 import lk.ijse.dep9.service.custom.UserService;
 
 public class UserServiceImpl implements UserService {
+
+
 }
