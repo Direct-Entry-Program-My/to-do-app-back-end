19dd8371f09664c19b4d177466e60d86f84bc579
danulatb <balsooriyadt@gmail.com>
2022-11-29 20:20:40 +0530

    connection util add

--- src/main/java/lk/ijse/dep9/service/custom/impl/UserServiceImpl.java
+++ src/main/java/lk/ijse/dep9/service/custom/impl/UserServiceImpl.java
@@ -1,13 +1,25 @@
 package lk.ijse.dep9.service.custom.impl;
 
+import lk.ijse.dep9.dao.DAOFactory;
+import lk.ijse.dep9.dao.custom.UserDAO;
 import lk.ijse.dep9.dto.UserDTO;
 import lk.ijse.dep9.service.custom.UserService;
 import lk.ijse.dep9.service.exception.AlreadyInUseException;
 import lk.ijse.dep9.service.exception.DuplicateException;
 import lk.ijse.dep9.service.exception.NotFoundException;
+import lk.ijse.dep9.service.util.Converter;
 
 public class UserServiceImpl implements UserService {
 
+    private final Converter converter;
+
+    private UserDAO userDAO;
+
+    public UserServiceImpl() {
+
+        this.converter = new Converter();
+        userDAO = DAOFactory.getInstance().getDAO(ConnectionU)
+    }
 
     @Override
     public void createNewUser(UserDTO dto) throws DuplicateException {
