9968e737bd9ad2d2f60de36e1cc0e0677b05efab
Eranga Bandara <erangabandara94@gmail.com>
2022-11-29 13:17:59 +0530

    Add doPatch to ToDOItemServlet

--- src/main/java/lk/ijse/dep9/api/ToDoItemServlet.java
+++ src/main/java/lk/ijse/dep9/api/ToDoItemServlet.java
@@ -5,11 +5,12 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lk.ijse.dep9.api.util.HTTPServlet2;
 
 import java.io.IOException;
 
 @WebServlet(name = "ToDoItemServlet", value = "/users/*/items/*", loadOnStartup = 0)
-public class ToDoItemServlet extends HttpServlet {
+public class ToDoItemServlet extends HTTPServlet2 {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -24,5 +25,10 @@ public class ToDoItemServlet extends HttpServlet {
     @Override
     protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         System.out.println("doDelete");
+    }
+
+    @Override
+    protected void doPatch(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("doPatch");
     }
 }
