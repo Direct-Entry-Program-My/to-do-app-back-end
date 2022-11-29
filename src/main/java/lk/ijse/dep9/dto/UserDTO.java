0cb8f890701cf95dbe5f0c28e6323b6666e1cf3d
danulatb <balsooriyadt@gmail.com>
2022-11-29 19:12:29 +0530

    user dto added

---
+++ src/main/java/lk/ijse/dep9/dto/UserDTO.java
@@ -0,0 +1,26 @@
+package lk.ijse.dep9.dto;
+
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserDTO {
+
+    @NotBlank
+    @Pattern(regexp = "u\\d{3}")
+    private String user_name;
+
+    @NotBlank
+    private String password;
+
+    @NotBlank
+    private String full_name;
+
+
+}
