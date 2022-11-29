080eaa76b333894f03e0ed096edaa0bd24c7c853
danulatb <balsooriyadt@gmail.com>
2022-11-29 11:36:19 +0530

    user table sql added

---
+++ src/main/resources/dbscript.sql
@@ -0,0 +1,5 @@
+CREATE TABLE User (
+                      user_name VARCHAR(50) PRIMARY KEY ,
+                      password VARCHAR(50) NOT NULL ,
+                      full_name VARCHAR(100) NOT NULL
+);
