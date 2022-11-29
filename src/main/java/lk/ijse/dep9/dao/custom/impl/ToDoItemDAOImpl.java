3c06d495e4f741d41b99fbde2455ab7497b56af0
Eranga Bandara <erangabandara94@gmail.com>
2022-11-29 13:52:35 +0530

    Add ConstraintViolationException

--- src/main/java/lk/ijse/dep9/dao/custom/impl/ToDoItemDAOImpl.java
+++ src/main/java/lk/ijse/dep9/dao/custom/impl/ToDoItemDAOImpl.java
@@ -3,19 +3,42 @@ package lk.ijse.dep9.dao.custom.impl;
 import lk.ijse.dep9.dao.custom.ToDoItemDAO;
 import lk.ijse.dep9.entity.ToDoItem;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 import java.util.Optional;
 
 public class ToDoItemDAOImpl implements ToDoItemDAO {
 
+    private final Connection connection;
 
-    @Override
-    public long count() {
-        return 0;
+    public ToDoItemDAOImpl(Connection connection) {
+        this.connection = connection;
     }
 
     @Override
-    public void deleteById(Integer PK) {
+    public long count() {
+        try {
+            PreparedStatement stm = connection.prepareStatement("SELECT COUNT(id) FROM ToDoItem");
+            ResultSet rst = stm.executeQuery();
+            rst.next();
+            return rst.getLong(1);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public void deleteById(Integer id) {
+        try {
+            PreparedStatement stm = connection.prepareStatement("DELETE FROM ToDoItem WHERE id = ?");
+            stm.setInt(1, id);
+            stm.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
 
     }
 
