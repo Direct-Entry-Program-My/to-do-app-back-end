f7ae8555db40ec2c8901c3feeda4c3594aa5b453
Eranga Bandara <erangabandara94@gmail.com>
2022-11-29 12:13:58 +0530

    Add ToDoItemServlet and postman-link.txt

--- src/main/java/lk/ijse/dep9/api/ToDoItemServlet.java
+++ src/main/java/lk/ijse/dep9/api/ToDoItemServlet.java
@@ -1,4 +1,28 @@
 package lk.ijse.dep9.api;
 
-public class ToDoItemServlet {
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet(name = "ToDoItemServlet", value = "/users/*/items/*", loadOnStartup = 0)
+public class ToDoItemServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("doGet");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("doPost");
+    }
+
+    @Override
+    protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("doDelete");
+    }
 }
